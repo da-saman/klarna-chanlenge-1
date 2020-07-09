bf6c2f393c112289ec5d058e056ead7a6e481778
saman darabi <saman.darabi@alopeyk.com>
2020-07-09 15:41:39 +0430

    final code design

--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
-// Calculate the balance in a specific category within the specified time period.
+// // Calculate the balance in a specific category within the specified time period.
 
-const transactions = [
-  {
-    id: 123,
-    sourceAccount: "my_account",
-    targetAccount: "coffee_shop",
-    amount: -30,
-    category: "eating_out",
-    time: "2018-03-12T12:34:00Z",
-  },
-];
+// const transactions = [
+//   {
+//     id: 123,
+//     sourceAccount: "my_account",
+//     targetAccount: "coffee_shop",
+//     amount: -30,
+//     category: "eating_out",
+//     time: "2018-03-12T12:34:00Z",
+//   },
+//   {
+//     id: 123,
+//     sourceAccount: "my_account",
+//     targetAccount: "coffee_shop",
+//     amount: -20,
+//     category: "eating_out",
+//     time: "2018-03-11T12:34:00Z",
+//   },
+//   {
+//     id: 123,
+//     sourceAccount: "my_account",
+//     targetAccount: "coffee_shop",
+//     amount: 10,
+//     category: "eating_out",
+//     time: "2018-04-10T12:34:00Z",
+//   },
+// ];
+const getInPeriod = (transactions, start, end) =>
+  transactions.filter((transaction) => {
+    const transactionDate = new Date(transaction.time);
+    return (
+      transactionDate.valueOf() >= start.valueOf() &&
+      transactionDate.valueOf() <= end.valueOf()
+    );
+  });
+
+const getInCategory = (transactions, category) =>
+  transactions.filter((transaction) => {
+    return transaction.category === category;
+  });
+
+const getBalance = (transactions) =>
+  transactions.reduce((acc, curr) => acc + curr.amount, 0);
 
 function getBalanceByCategoryInPeriod(transactions = [], category, start, end) {
-  // add your code here
-  return 0;
+  const inPeriod = getInPeriod(transactions, start, end);
+  const inCategoryAndPeriod = getInCategory(inPeriod, category);
+  return getBalance(inCategoryAndPeriod);
 }
 
 exports.getBalanceByCategoryInPeriod = getBalanceByCategoryInPeriod;
--- test/unitTest.js
+++ test/unitTest.js
@@ -2,6 +2,33 @@ const assert = require("chai").assert;
 
 const { getBalanceByCategoryInPeriod } = require("../src/index");
 
+const transactions = [
+  {
+    id: 123,
+    sourceAccount: "my_account",
+    targetAccount: "coffee_shop",
+    amount: -30,
+    category: "eating_out",
+    time: "2018-03-12T12:34:00Z",
+  },
+  {
+    id: 123,
+    sourceAccount: "my_account",
+    targetAccount: "coffee_shop",
+    amount: -20,
+    category: "eating_out",
+    time: "2018-03-11T12:34:00Z",
+  },
+  {
+    id: 123,
+    sourceAccount: "my_account",
+    targetAccount: "coffee_shop",
+    amount: 10,
+    category: "groceries",
+    time: "2018-03-10T12:34:00Z",
+  },
+];
+
 describe("getBalanceByCategoryInPeriod()", function () {
   it("returns 0 if there are no transactions", function () {
     assert.equal(
@@ -12,6 +39,18 @@ describe("getBalanceByCategoryInPeriod()", function () {
         new Date("2018-03-31")
       ),
       0
+    );
+  });
+
+  it("returns sum of transactions in time period", function () {
+    assert.equal(
+      getBalanceByCategoryInPeriod(
+        transactions,
+        "groceries",
+        new Date("2018-03-01"),
+        new Date("2018-03-31")
+      ),
+      10
     );
   });
 
