2337ba1b33195585df1abbf87c78766c43301e42
saman darabi <saman.darabi@alopeyk.com>
2020-07-10 14:39:33 +0430

    first submition stage

--- src/index.js
+++ src/index.js
@@ -26,12 +26,12 @@
 //     time: "2018-04-10T12:34:00Z",
 //   },
 // ];
-const getInPeriod = (transactions, start, end) =>
+const getInTimePeriod = (transactions, start, end) =>
   transactions.filter((transaction) => {
     const transactionDate = new Date(transaction.time);
     return (
       transactionDate.valueOf() >= start.valueOf() &&
-      transactionDate.valueOf() <= end.valueOf()
+      transactionDate.valueOf() < end.valueOf()
     );
   });
 
@@ -44,9 +44,13 @@ const getBalance = (transactions) =>
   transactions.reduce((acc, curr) => acc + curr.amount, 0);
 
 function getBalanceByCategoryInPeriod(transactions = [], category, start, end) {
-  const inPeriod = getInPeriod(transactions, start, end);
+  const inPeriod = getInTimePeriod(transactions, start, end);
   const inCategoryAndPeriod = getInCategory(inPeriod, category);
   return getBalance(inCategoryAndPeriod);
 }
+console.log(getInCategory([], ""));
 
 exports.getBalanceByCategoryInPeriod = getBalanceByCategoryInPeriod;
+exports.getBalance = getBalance;
+exports.getInCategory = getInCategory;
+exports.getInTimePeriod = getInTimePeriod;
--- test/unitTest.js
+++ test/unitTest.js
@@ -1,6 +1,12 @@
-const assert = require("chai").assert;
+const { assert, expect } = require("chai");
 
-const { getBalanceByCategoryInPeriod } = require("../src/index");
+const {
+  getBalanceByCategoryInPeriod,
+  getBalance,
+  getInCategory,
+  getInTimePeriod,
+} = require("../src/index");
+const { it } = require("mocha");
 
 const transactions = [
   {
@@ -9,7 +15,7 @@ const transactions = [
     targetAccount: "coffee_shop",
     amount: -30,
     category: "eating_out",
-    time: "2018-03-12T12:34:00Z",
+    time: "2018-03-16T12:34:00Z",
   },
   {
     id: 123,
@@ -17,7 +23,7 @@ const transactions = [
     targetAccount: "coffee_shop",
     amount: -20,
     category: "eating_out",
-    time: "2018-03-11T12:34:00Z",
+    time: "2018-03-14T12:34:00Z",
   },
   {
     id: 123,
@@ -42,6 +48,30 @@ describe("getBalanceByCategoryInPeriod()", function () {
     );
   });
 
+  it("returns 0 if there are no transactions in category", function () {
+    assert.equal(
+      getBalanceByCategoryInPeriod(
+        transactions,
+        "play",
+        new Date("2018-03-01"),
+        new Date("2018-03-31")
+      ),
+      0
+    );
+  });
+
+  it("returns 0 if there are no transactions in period", function () {
+    assert.equal(
+      getBalanceByCategoryInPeriod(
+        transactions,
+        "eating_out",
+        new Date("2018-03-20"),
+        new Date("2018-03-31")
+      ),
+      0
+    );
+  });
+
   it("returns sum of transactions in time period", function () {
     assert.equal(
       getBalanceByCategoryInPeriod(
@@ -53,6 +83,119 @@ describe("getBalanceByCategoryInPeriod()", function () {
       10
     );
   });
-
   // add your tests here
+});
+
+describe("getBalance()", function () {
+  it("returns 0 if there is no transaction", function () {
+    assert.equal(getBalance([]), 0);
+  });
+
+  it("returns balance of transactions", function () {
+    assert.equal(getBalance(transactions), -40);
+  });
+});
+
+describe("getInCategory()", function () {
+  it("returns [] if there is no transaction", function () {
+    expect(getInCategory([], "eating_out")).to.eql([]);
+  });
+
+  it("returns [] if there is no such category", function () {
+    expect(getInCategory(transactions, "play")).to.eql([]);
+  });
+
+  it("returns proper array ", function () {
+    expect(getInCategory(transactions, "eating_out")).to.eql([
+      {
+        id: 123,
+        sourceAccount: "my_account",
+        targetAccount: "coffee_shop",
+        amount: -30,
+        category: "eating_out",
+        time: "2018-03-16T12:34:00Z",
+      },
+      {
+        id: 123,
+        sourceAccount: "my_account",
+        targetAccount: "coffee_shop",
+        amount: -20,
+        category: "eating_out",
+        time: "2018-03-14T12:34:00Z",
+      },
+    ]);
+  });
+});
+
+describe("getInTimePeriod()", function () {
+  it("returns [] if there is no transaction", function () {
+    expect(
+      getInTimePeriod([], new Date("2018-03-01"), new Date("2018-03-31"))
+    ).to.eql([]);
+  });
+
+  it("returns [] if there is no transaction in period", function () {
+    expect(
+      getInTimePeriod(
+        transactions,
+        new Date("2018-03-20"),
+        new Date("2018-03-31")
+      )
+    ).to.eql([]);
+  });
+
+  it("returns [] if start > end", function () {
+    expect(
+      getInTimePeriod(
+        transactions,
+        new Date("2018-03-31"),
+        new Date("2018-03-20")
+      )
+    ).to.eql([]);
+  });
+  it("inclusive start and exclusive end", function () {
+    expect(
+      getInTimePeriod(
+        transactions,
+        new Date("2018-03-10T12:34:00Z"),
+        new Date("2018-03-14T12:34:00Z")
+      )
+    ).to.eql([
+      {
+        id: 123,
+        sourceAccount: "my_account",
+        targetAccount: "coffee_shop",
+        amount: 10,
+        category: "groceries",
+        time: "2018-03-10T12:34:00Z",
+      },
+    ]);
+  });
+
+  it("returns proper array ", function () {
+    expect(
+      getInTimePeriod(
+        transactions,
+        new Date("2018-03-14"),
+        new Date("2018-03-17")
+      )
+    ).to.eql([
+      {
+        id: 123,
+        sourceAccount: "my_account",
+        targetAccount: "coffee_shop",
+        amount: -30,
+        category: "eating_out",
+        time: "2018-03-16T12:34:00Z",
+      },
+      {
+        id: 123,
+        sourceAccount: "my_account",
+        targetAccount: "coffee_shop",
+        amount: -20,
+        category: "eating_out",
+        time: "2018-03-14T12:34:00Z",
+      },
+    ]);
+  });
 });
